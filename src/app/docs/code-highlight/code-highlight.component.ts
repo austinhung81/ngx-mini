981493e5edce20cf06a35414e5e5e914253106f9
Wilson Su <wilson_su@trend.com.tw>
2018-08-03 10:54:02 +0800

    revise CodeHighlightComponent using prism component

--- src/app/docs/code-highlight/code-highlight.component.ts
+++ src/app/docs/code-highlight/code-highlight.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import * as Prism from 'prismjs';
 
 @Component({
   selector: 'code-highlight',
@@ -8,17 +7,7 @@ import * as Prism from 'prismjs';
 })
 export class CodeHighlightComponent implements OnInit {
   @Input() lang = 'javascript';
-
-  private _code: string;
-
-  @Input()
-  set code(value: string) {
-    this._code = Prism.highlight(value, Prism.languages[this.lang]);
-  }
-
-  get code() {
-    return this._code;
-  }
+  @Input() code;
 
   ngOnInit() {
   }
